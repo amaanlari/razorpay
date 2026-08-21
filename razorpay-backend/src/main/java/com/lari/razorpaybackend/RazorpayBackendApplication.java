ac4481f4e71df7a0289a48f7a95115dd76beb15b
Amaan Lari <121742871+amaanlari@users.noreply.github.com>
2026-08-21 15:08:34 +0000

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ razorpay-backend/src/main/java/com/lari/razorpaybackend/RazorpayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lari.razorpaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazorpayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazorpayBackendApplication.class, args);
+	}
+
+}
---
+++ razorpay-backend/src/test/java/com/lari/razorpaybackend/RazorpayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lari.razorpaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazorpayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
